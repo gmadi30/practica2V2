1b77317625f1febae088b5ad940efd3b11157ae9
gmadi30 <georgesmadi30@hotmail.com>
2021-02-12 13:29:55 +0100

    Prueba Git en un nuevo repositorio local y remoto

---
+++ Array1.java
@@ -0,0 +1,24 @@
+package cadenas;
+
+import java.util.*;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		
+		int contadorLetra;
+		
+		String frase;
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Introduce una frase: ");
+		
+		frase = entrada.next();
+		
+		frase = frase.toLowerCase();
+	
+			//Hola
+	}
+
+}
